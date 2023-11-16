586ae3e10ddc8b9ad61e36cc1da956a6e04fa693
Sydney Balboni <106030271+sydneybalboni@users.noreply.github.com>
2023-11-15 21:13:48 -0600

    Modify frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css'; // Import your CSS file for styling
 
 function Chatbot() {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
+  const chatContainerRef = useRef(null);
 
   const sendMessage = async () => {
     if (!input.trim()) return; // Avoid sending empty messages
@@ -41,12 +42,19 @@ function Chatbot() {
     setInput(''); // Clear the input field
   };
 
+  // Scroll the chat container to the bottom when messages change
+  useEffect(() => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+    }
+  }, [messages]);
+
   return (
     <div className="chat-container">
       <div className="chat-header">
-        <img src="..\public\logo.png" alt="Logo" className="header-image" />
+        <img src="logo.png" alt="Header Image" className="header-image" />
       </div>
-      <div className="chat-messages">
+      <div className="chat-messages" ref={chatContainerRef}>
         {messages.map((msg, index) => (
           <div
             key={index}
